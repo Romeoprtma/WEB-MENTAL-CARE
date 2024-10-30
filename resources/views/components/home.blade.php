f0e2810555a787624a08403a4cc5af290d0375d2
Romeoprtma <romeomuliap@gmail.com>
2024-10-30 10:59:46 +0700

    second commit

---
+++ resources/views/components/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@section('home')
+    <section class="flex justify-center items-center bg-[#756AB6]">
+        <div class="title-hero">
+            <h1>MentalCare</h1>
+            <h3>Dukungan Kesehatan Mental yang Terjangkau untuk Semua</h3>
+        </div>
+        <div class="img-hero mt-10">
+            <img src="img/icon_hero.svg" width="481px" height="469px" alt="">
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Mental Care</title>
+</head>
+<body>
+    @include('partials.navbar')
+    <div>
+        @yield('home')
+    </div>
+    @include('partials.footer')
+</body>
+</html>
