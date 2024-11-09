e8605a132764c9240e9f74b752bef412963ad046
Romeoprtma <romeomuliap@gmail.com>
2024-11-09 21:32:28 +0700

    change navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('components.home');
 });
+
+Route::get('/admin', function () {
+    return view('components.homeAdmin');
+});
