a829b0ef387e607bc3e80b802ec9b609fe8a226b
unknown <ahmaddzulfikri003@gmail.com>
2024-11-09 19:36:23 +0700

    tambah tombol masuk

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('home')
-    <section class="flex justify-center items-center bg-[#756AB6]">
+    <section class="flex justify-center items-center bg-[#756AB6] ">
         <div class="title-hero">
             <h1>MentalCare</h1>
             <h3>Dukungan Kesehatan Mental yang Terjangkau untuk Semua</h3>
